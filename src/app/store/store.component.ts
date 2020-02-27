59e2edbce0bd16b3f65c246a6bd1f1a1895abe09
dbryant007 <dbryant007@hotmail.com>
2020-02-26 22:43:32 -0700

    1 file modified

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Product } from "../model/product.model";
+import { Product } from "./model/product.model";
 import { ProductRepository } from "../model/product.repository";
 
 @Component({
